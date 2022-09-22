43f81194e0a59b2211efca6c1d3027408c72a4ce
Ivar Elgsæther Haugland <ivareh@stud.ntnu.no>
2022-09-22 14:33:18 +0200

    Added simple container

--- gitlab-app/src/App.tsx
+++ gitlab-app/src/App.tsx
@@ -1,9 +1,15 @@
-function App() {
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+export default function SimpleContainer() {
   return (
-    <div className="container">
-      
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+      </Container>
+    </React.Fragment>
   );
 }
-
-export default App;
