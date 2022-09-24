8b0a97ccf33b52a89c0e5450055177fd9835d347
ivareh <ivareh.stud.ntnu.no>
2022-09-24 14:40:07 +0200

    Created custom app bar theme that resembles Gitlab's themes

---
+++ gitlab-app/src/themes/GitLabTheme.tsx
@@ -0,0 +1,49 @@
+/* Added to support Typescript, see https://mui.com/material-ui/customization/palette/#adding-new-colors */
+import { createTheme } from '@mui/material/styles';
+declare module '@mui/material/styles' {
+    interface Theme {
+      status: {
+        danger: React.CSSProperties['color'];
+      };
+    }
+  
+    interface Palette {
+      neutral: Palette['primary'];
+    }
+    interface PaletteOptions {
+      neutral: PaletteOptions['primary'];
+    }
+  
+    interface PaletteColor {
+      darker?: string;
+    }
+    interface SimplePaletteColorOptions {
+      darker?: string;
+    }
+    interface ThemeOptions {
+      status: {
+        danger: React.CSSProperties['color'];
+      };
+    }
+  }
+
+
+
+const GitLabTheme = createTheme({
+  status: {
+    danger: '#ff6b27',
+  },
+  palette: {
+    primary: {
+      main: '#544389',
+      darker: '#e24328',
+    },
+    neutral: {
+      main: '#ffa427',
+      contrastText: '#fff',
+    },
+  },
+});
+
+
+export default GitLabTheme
