251f44767a7fef7bdff11525fba47eca1de47468
ivareh <ivareh.stud.ntnu.no>
2022-09-24 17:01:11 +0200

    Created custom gitlab icon (GitLabIcon.tsx) and added it to toolbar in GitLabAppBar

--- gitlab-app/src/components/GitLabAppBar.tsx
+++ gitlab-app/src/components/GitLabAppBar.tsx
@@ -1,5 +1,6 @@
-import { AppBar, AppBarProps, ThemeProvider, Toolbar, Typography } from '@mui/material'
+import { AppBar, AppBarProps, IconButton, ThemeProvider, Toolbar, Typography } from '@mui/material'
 import GitLabTheme from '../themes/GitLabTheme'
+import GitLabIcon from './GitLabIcon'
 
 /* Customised App Bar */
 function GitLabAppBar(props: AppBarProps){ {
@@ -8,9 +9,12 @@ function GitLabAppBar(props: AppBarProps){ {
         <ThemeProvider theme={GitLabTheme}>
             <AppBar>
                 <Toolbar>
-                <Typography variant="h6" align="center" color="white">
-                    Gitlab Repo Dataloader
-                </Typography>
+                  <IconButton>
+                    <GitLabIcon htmlColor="#e24329" fontSize="large"></GitLabIcon>
+                  </IconButton>
+                  <Typography variant="h6" align="center" color="white" sx={{marginLeft:2}}>
+                      Gitlab Repo Dataloader
+                  </Typography>
                 </Toolbar>
             </AppBar>
         </ThemeProvider>
