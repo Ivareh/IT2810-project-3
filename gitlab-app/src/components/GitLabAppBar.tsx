f606e5fc489ff71e6f8186a6af1a32ffa86e0002
ivareh <ivareh.stud.ntnu.no>
2022-09-25 20:45:31 +0200

    Added navigation to main page when clicking icon in tool bar

--- gitlab-app/src/components/GitLabAppBar.tsx
+++ gitlab-app/src/components/GitLabAppBar.tsx
@@ -1,24 +1,26 @@
-import { AppBar, AppBarProps, IconButton, ThemeProvider, Toolbar, Typography } from '@mui/material'
+import { AppBar, AppBarProps, IconButton, ThemeProvider, Toolbar } from '@mui/material'
 import GitLabTheme from '../themes/GitLabTheme'
-import GitLabIcon from './GitLabIcon'
+import GitLabIcon from './custom-icons/GitLabIcon'
 import Menu from './Menu'
+import {useNavigate} from 'react-router-dom'
+
+
 
 /* Customised App Bar */
 function GitLabAppBar(props: AppBarProps){ {
+  const navigate = useNavigate()
+
   return (
-    <div>
-        <ThemeProvider theme={GitLabTheme}>
-            <AppBar>
-                <Toolbar>
-                  <IconButton>
-                    <GitLabIcon htmlColor="#e24329" fontSize="large"></GitLabIcon>
-                  </IconButton>
-                  <Menu></Menu>
-                </Toolbar>
-            </AppBar>
-        </ThemeProvider>
-    </div>
-    
+    <ThemeProvider theme={GitLabTheme}>
+        <AppBar>
+            <Toolbar>
+              <IconButton onClick={() => navigate("/")}>
+                <GitLabIcon htmlColor="#e24329" fontSize="large"></GitLabIcon>
+              </IconButton>
+              <Menu></Menu>
+            </Toolbar>
+        </AppBar>
+    </ThemeProvider>
   )}
 }
 
