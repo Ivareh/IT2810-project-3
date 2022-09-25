a71f41d5bf49829c1a05855e3d34005b631a8191
ivareh <ivareh.stud.ntnu.no>
2022-09-25 17:10:56 +0200

    Created gitlabdata component (not finished)

---
+++ gitlab-app/src/components/GitLabData.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/system"
+
+
+
+
+/* Displays Git Lab Data after it is loaded */
+function GitLabData() {
+  return (
+    <>
+{/*     <Box border={1} height={80%} width={80%}>
+
+    </Box> */}
+    </>
+  )
+}
+
+export default GitLabData
