eb684d36bd0921c5fb3f163fc4a89c77aa555929
vnhenrik <vnhenrik@stud.ntnu.no>
2022-09-28 08:19:58 +0200

    Added try catch statements to the fetch request for error handling

--- gitlab-app/src/components/InputForm.tsx
+++ gitlab-app/src/components/InputForm.tsx
@@ -3,6 +3,7 @@ import SendIcon from '@mui/icons-material/Send';
 import Button from '@mui/material/Button';
 import {useState} from "react";
 
+
 export default function InputForm() {
     const [domain, setDomain] = useState('https://gitlab.stud.idi.ntnu.no/api/v4/projects/')
     const [projectId, setProjectId] = useState('17598')
@@ -14,31 +15,76 @@ export default function InputForm() {
         e.preventDefault()
         await fetchIssues()
         await fetchCommits()
-        console.log(issues)
-        console.log(commits)
 
     }
+
+
+    /* const commitsStructure = [
+         {
+             0:
+                 {
+                     id: '8f8f742368b068cd1bc5855c51e261e93c366488',
+                     short_id: '8f8f7423',
+                     created_at: '2022-09-26T21:42:50.000+02:00',
+                     parent_ids: Array(2),
+                     title: "Merge branch 'revert-dd2c9307' into 'main'",
+                 }
+         }, {
+             1:
+                 {
+                     id: '4d5e1faf40bda73a23a28c95a5a6ff7d377b1b97',
+                     short_id: '4d5e1faf',
+                     created_at: '2022-09-26T21:41:48.000+02:00',
+                     parent_ids: Array(1),
+                     title: 'Revert "Created dynamic fetch functions for' +
+                         ' getting issues and commits. As',
+                 }
+         }
+     ]*/
+
+    /*    const issueStructure = [
+            {
+                id: 76903,
+                iid: 6,
+                project_id: 17598,
+                title: 'Fetch request med AJAX',
+                description: 'Skriv fetch request med AJAX opp mot rest-api til' +
+                    ' gitlab',
+            }
+        ]*/
 
     const fetchIssues = async () => {
-        await fetch(`${domain}/${projectId}/issues?private_token=${token}`)
-            .then((res) => res.json())
-            .then((result) => {
-                setIssues(result)
-            })
+        try {
+            await fetch(`${domain}/${projectId}/issues?private_token=${token}`)
+                .then((res) => res.json())
+                .then((result) => {
+                    setIssues(result)
+                })
+        } catch (e) {
+            console.log(e)
+
+        }
+
     }
     const fetchCommits = async () => {
-        await fetch(`${domain}/${projectId}/repository/commits?private_token=${token}`)
-            .then((res) => res.json())
-            .then((result) => {
-                setCommits(result)
-            })
+        try {
+            await fetch(`${domain}/${projectId}/repository/commits?private_token=${token}`)
+                .then((res) => res.json())
+                .then((result) => {
+                    setCommits(result)
+                })
+
+        } catch (e) {
+            console.log(e)
+        }
+
     }
 
     return (
         <div>
             <form
                 onSubmit={handleSubmit}
-                style={{display: "flex"}}>
+                style={{display: "flex", flexFlow: 'row nowrap'}}>
                 <TextField id="outlined-search"
                            fullWidth
                            label="Domain"
@@ -50,7 +96,7 @@ export default function InputForm() {
                 />
                 <TextField id="gitlab-id"
                            label="Project id"
-                           sx={{minWidth: '200px'}}
+                           sx={{minWidth: '100px'}}
                            type="number"
                            value={projectId}
                            onChange={(event) => {
@@ -73,10 +119,10 @@ export default function InputForm() {
                     Get
                 </Button>
             </form>
-
+            {/*
             {issues.map((issue) => (
                 <p key={issue.iid}>{issue.title}</p>
-            ))}
+            ))}*/}
         </div>
     )
 
