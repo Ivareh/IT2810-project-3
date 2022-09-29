75fab46a7b053e828db69f589d7994130adda9bd
Veronika Stengl <git@sten.gl>
2022-09-29 15:29:49 +0200

    restructure filter forms
    make two seperate filter forms
    add toogle hide/view commits and issues

---
+++ gitlab-app/src/components/FilterFormIssue.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { Box } from "@mui/system"
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+
+export default function BasicSelect() {
+
+ /* const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+  */
+
+  return (
+  <>
+    <Box sx={{ minWidth: 120 }}>
+      <h2>Filter</h2>
+      <FormControl fullWidth>
+        <InputLabel>Filter</InputLabel>
+        <Select
+        >
+          <MenuItem value={10}>Title</MenuItem>
+          <MenuItem value={20}>Description</MenuItem>
+          <MenuItem value={30}>State</MenuItem>
+          <MenuItem value={40}>Before</MenuItem>
+          <MenuItem value={50}>After</MenuItem>
+        </Select>
+      </FormControl>
+      <TextField id="outlined-search"
+        fullWidth
+        label="Value"
+        type="text" />
+
+    </Box><Box my={1} display="flex" justifyContent="flex-end">
+        <Button variant="contained">Filter</Button>
+    </Box>
+    </>
+  );
+}
--- gitlab-app/src/components/GitLabRepo.tsx
+++ gitlab-app/src/components/GitLabRepo.tsx
@@ -2,47 +2,48 @@ import { Container, Typography, Button } from "@mui/material"
 import { Box } from "@mui/system"
 import GitLabCommitDataTable from "./GitLabCommitDataTable"
 import GitLabIssueDataTable from "./GitLabIssueDataTable"
-import FilterForm from "./FilterForm"
+import FilterFormCommit from "./FilterFormCommit"
+import FilterFormIssue from "./FilterFormIssue"
+import { useState } from 'react'
 /* import {CommitItem} from "./interface/DataFormat" */
-
 
 /* Displays Git Lab Data after it is loaded in a table with parameters*/
 function GitLabRepo() {
+  const [showCommits, setShowCommits] = useState(true);
+  const [showIssues, setShowIssues] = useState(false);
   return (
 
     <Container maxWidth="lg">
       <Box mt={10} >
         <Typography align="left" variant="h4" fontWeight={500} gutterBottom > GitLab Repo </Typography>
 
-        <Box>
-          <h2>Filter</h2>
-          <Box>
-            <FilterForm/>
-          </Box>
-          <Box my={1} display="flex" justifyContent="flex-end">
-            <Button variant="contained">Filter</Button>
-          </Box>
+        <>
+          <Button variant = "contained" onClick={() => setShowCommits(prev => !prev)}>view commits</Button>
+          {showCommits && <Box>
+            <FilterFormCommit/>
+            <h2>Commits</h2>
+            <Box border={1} mb={10} width="sm">
+            {/* Table to display data */}
+            <GitLabCommitDataTable/>
         </Box>
+          </Box>}
+        </>
 
-        <Box my={1} display="flex" justifyContent="space-evenly">
-          <Button variant="contained">View Commits</Button>
-          <Button variant="contained">View Issues</Button>
+        <>
+        <Button variant = "contained" onClick={() => setShowIssues(prev => !prev)}>view issues</Button>
+          {showIssues && <Box>
+            <FilterFormIssue/>
+            <h2>Issues</h2>
+            <Box border={1} width="sm">
+            {/* Table to display data */}
+            <GitLabIssueDataTable/>
         </Box>
+          </Box>}
+        </>
 
-        <h2>Commits</h2>
-        <Box border={1} mb={10} width="sm">
-          {/* Table to display data */}
-          <GitLabCommitDataTable/>
-        </Box>
-        <h2>Issues</h2>
-        <Box border={1} width="sm">
-          <GitLabIssueDataTable/>
-        </Box>
- 
       </Box>
     </Container>
   )
 }
-
 
 export default GitLabRepo
