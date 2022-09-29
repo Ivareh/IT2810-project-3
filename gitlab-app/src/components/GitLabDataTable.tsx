5062e3b0ab0afeab2c1919963132ea43de28dd51
vnhenrik <vnhenrik@stud.ntnu.no>
2022-09-29 11:30:52 +0200

    Stored data from fetch request in localStorage, use this data to display information in tables

--- gitlab-app/src/components/GitLabDataTable.tsx
+++ gitlab-app/src/components/GitLabDataTable.tsx
@@ -1,59 +1,108 @@
-import {TableContainer, Table, TableHead, TableRow, TableBody, Paper, TableCell} from '@mui/material';  
-import { useEffect, useState } from 'react';
- import SampleCommitData from './data/SampleCommitData'; 
+import {
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from '@mui/material';
+import {useEffect, useState} from 'react';
+import SampleCommitData from './data/SampleCommitData';
 import {CommitItem} from "./interface/DataFormat"
 
 
-function getPropValues(){
-  let data : Array<CommitItem> = []
-  let sampleData = SampleCommitData;
-  sampleData.forEach((item) => {
-    item.forEach((value) => {
-      data.push(value)
+function getPropValues() {
+    let data: Array<CommitItem> = []
+    let sampleData = SampleCommitData;
+    sampleData.forEach((item) => {
+        item.forEach((value) => {
+            data.push(value)
+        })
     })
-  })
-  return data
+    return data
 }
 
 /* Table, inspired by https://www.youtube.com/watch?v=qk2oY7W3fuY*/
-function GitLabDataTable (){
-  const [tableData, setTableData] = useState([]);
-  
-  useEffect (() => {
-    let data : CommitItem[] = getPropValues()
-    setTableData(data)
-  },[])
+function GitLabDataTable() {
+    const [tableData, setTableData] = useState([]);
+    const [issues, setIssues] = useState([]);
+    const [commits, setCommits] = useState([]);
 
-  return (
-    <TableContainer component={Paper} sx={{maxHeight:400}}>
-        <Table aria-label="simple table" stickyHeader>
-            <TableHead>
-              <TableRow>
-                <TableCell>Title</TableCell>
-                <TableCell>Author name</TableCell>
-                <TableCell>Committer name</TableCell>
-                <TableCell align='center'>Committed date</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {tableData.map((row => 
-                  <TableRow 
-                    key={row.id}
-                    sx = { { '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell width={600}>{row.title}</TableCell>
-                    <TableCell>{row.author_name}</TableCell>
-                    <TableCell>{row.committer_name}</TableCell>
-                    <TableCell align='center'>{row.committed_date}</TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-        </Table>
-    </TableContainer>
-  )
+    function getIssues() {
+        const issues = localStorage.getItem("issues");
+        const initialValueIssues = JSON.parse(issues);
+        return initialValueIssues || "";
+    }
+
+    function getCommits() {
+        const commits = localStorage.getItem("commits");
+        const initialValueIssues = JSON.parse(commits);
+        return initialValueIssues || "";
+
+    }
+
+    function printData(): void {
+        if (localStorage.getItem('commits') || localStorage.getItem('issues')) {
+            console.log(commits)
+            console.log(issues)
+            // the code under shows size of local storage.
+            // https://stackoverflow.com/questions/4391575/how-to-find-the-size-of-localstorage
+            let _lsTotal = 0, _xLen, _x;
+            for (_x in localStorage) {
+                if (!localStorage.hasOwnProperty(_x)) {
+                    continue;
+                }
+                _xLen = ((localStorage[_x].length + _x.length) * 2);
+                _lsTotal += _xLen;
+                console.log(_x.substr(0, 50) + " = " + (_xLen / 1024).toFixed(2) + " KB")
+            }
+            console.log("Total = " + (_lsTotal / 1024).toFixed(2) + " KB");
+        } else {
+            alert('Data not loaded.')
+        }
+    }
+
+    useEffect(() => {
+        setIssues(getIssues())
+        setCommits(getCommits())
+        let data: CommitItem[] = getPropValues()
+        setTableData(data)
+    }, [])
+
+    return (
+        <TableContainer component={Paper} sx={{maxHeight: 400}}>
+            <button onClick={() => printData()}>Print data and size of localStorage
+            </button>
+            <Table aria-label="simple table" stickyHeader>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Title</TableCell>
+                        <TableCell>Author name</TableCell>
+                        <TableCell>Committer name</TableCell>
+                        <TableCell align='center'>Committed date</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {commits.map((commit =>
+                            <TableRow
+                                key={commit.short_id}
+                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                            >
+                                <TableCell
+                                    width={600}>{commit.title}</TableCell>
+                                <TableCell>{commit.author_name}</TableCell>
+                                <TableCell
+                                    align='center'>{commit.committer_name}</TableCell>
+                                <TableCell
+                                    align='center'>{commit.committed_date}</TableCell>
+                            </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
 }
 
-
-  
 
 export default GitLabDataTable
--- gitlab-app/src/components/InputForm.tsx
+++ gitlab-app/src/components/InputForm.tsx
@@ -2,84 +2,58 @@ import TextField from '@mui/material/TextField';
 import SendIcon from '@mui/icons-material/Send';
 import Button from '@mui/material/Button';
 import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 
 export default function InputForm() {
     const [domain, setDomain] = useState('https://gitlab.stud.idi.ntnu.no/api/v4/projects/')
     const [projectId, setProjectId] = useState('17598')
     const [token, setToken] = useState('glpat-2vnnyXYGFDt9YKYu5QYW')
-    const [issues, setIssues] = useState([])
-    const [commits, setCommits] = useState([])
+    const navigate = useNavigate()
+    let [data, setData] = useState(Object)
 
     const handleSubmit = async (e: { preventDefault: () => void; }) => {
         e.preventDefault()
-        await fetchIssues()
-        await fetchCommits()
-        console.log(issues)
-        console.log(commits)
+        fetchCommits().then((data) => localStorage.setItem('commits',
+            JSON.stringify(data))).then(() =>
+            alert('Data loaded!'))
+        fetchIssues().then((data) => setData(data)).then(() =>
+            localStorage.setItem('issues', JSON.stringify(data))).then(() =>
+            navigate('/gitlab-repo'))
+
 
     }
 
 
-    /* const commitsStructure = [
-         {
-             0:
-                 {
-                     id: '8f8f742368b068cd1bc5855c51e261e93c366488',
-                     short_id: '8f8f7423',
-                     created_at: '2022-09-26T21:42:50.000+02:00',
-                     parent_ids: Array(2),
-                     title: "Merge branch 'revert-dd2c9307' into 'main'",
-                 }
-         }, {
-             1:
-                 {
-                     id: '4d5e1faf40bda73a23a28c95a5a6ff7d377b1b97',
-                     short_id: '4d5e1faf',
-                     created_at: '2022-09-26T21:41:48.000+02:00',
-                     parent_ids: Array(1),
-                     title: 'Revert "Created dynamic fetch functions for' +
-                         ' getting issues and commits. As',
-                 }
-         }
-     ]*/
-
-    /*    const issueStructure = [
-            {
-                id: 76903,
-                iid: 6,
-                project_id: 17598,
-                title: 'Fetch request med AJAX',
-                description: 'Skriv fetch request med AJAX opp mot rest-api til' +
-                    ' gitlab',
+    const fetchIssues = async (): Promise<object> => {
+        try {
+            const url = `${domain}/${projectId}/issues?private_token=${token}`
+            const res = await fetch(url)
+            if (res.ok) {
+                return await res.json()
+            } else {
+                alert("Something went wrong!")
             }
-        ]*/
 
-    const fetchIssues = async () => {
-        try {
-            await fetch(`${domain}/${projectId}/issues?private_token=${token}`)
-                .then((res) => res.json())
-                .then((result) => {
-                    setIssues(result)
-                })
         } catch (e) {
             console.log(e)
 
         }
-
     }
-    const fetchCommits = async () => {
+
+    const fetchCommits = async (): Promise<object> => {
         try {
-            await fetch(`${domain}/${projectId}/repository/commits?private_token=${token}`)
-                .then((res) => res.json())
-                .then((result) => {
-                    setCommits(result)
-                })
+            const url = `${domain}/${projectId}/repository/commits?private_token=${token}`
+            const res = await fetch(url)
+            if (res.ok) {
+                return await res.json()
+            }
+
 
         } catch (e) {
             console.log(e)
-        }
 
+        }
     }
 
     return (
@@ -121,10 +95,6 @@ export default function InputForm() {
                     Get
                 </Button>
             </form>
-            {/*
-            {issues.map((issue) => (
-                <p key={issue.iid}>{issue.title}</p>
-            ))}*/}
         </div>
     )
 
