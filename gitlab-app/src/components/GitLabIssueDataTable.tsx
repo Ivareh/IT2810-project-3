8cce07bdd970a205c3df6d8efd5d1390b4625613
Vegard <vnhenrik@stud.ntnu.no>
2022-09-29 14:08:04 +0200

    Formatting

--- gitlab-app/src/components/GitLabIssueDataTable.tsx
+++ gitlab-app/src/components/GitLabIssueDataTable.tsx
@@ -1,25 +1,32 @@
-import {TableContainer, Table, TableHead, TableRow, TableBody, Paper, TableCell} from '@mui/material';  
-import { useEffect, useState } from 'react';
- import SampleIssueData from './data/SampleIssueData'; 
+import {
+    TableContainer,
+    Table,
+    TableHead,
+    TableRow,
+    TableBody,
+    Paper,
+    TableCell
+} from '@mui/material';
+import {useEffect, useState} from 'react';
+import SampleIssueData from './data/SampleIssueData';
 import {CommitItem, IssueItem} from "./interface/DataFormat"
 
 
-function getPropValues(){
-  let data : Array<IssueItem> = []
-  let sampleData = SampleIssueData;
-  sampleData.forEach((item) => {
-    item.forEach((value) => {
-      data.push(value)
+function getPropValues() {
+    let data: Array<IssueItem> = []
+    let sampleData = SampleIssueData;
+    sampleData.forEach((item) => {
+        item.forEach((value) => {
+            data.push(value)
+        })
     })
-  })
-  return data
+    return data
 }
 
 /* Table, inspired by https://www.youtube.com/watch?v=qk2oY7W3fuY*/
-function GitLabIssueDataTable (){
-  const [tableData, setTableData] = useState([]);
+function GitLabIssueDataTable() {
+    const [tableData, setTableData] = useState([]);
     const [issues, setIssues] = useState([]);
-
 
 
     function getIssues() {
@@ -27,7 +34,6 @@ function GitLabIssueDataTable (){
         const initialValueIssues = JSON.parse(issues);
         return initialValueIssues || "";
     }
-
 
 
     function printData(): void {
@@ -57,36 +63,35 @@ function GitLabIssueDataTable (){
     }, [])
 
 
-  return (
-    <TableContainer component={Paper} sx={{maxHeight:400}}>
-        <Table aria-label="simple table" stickyHeader>
-            <TableHead>
-              <TableRow>
-                <TableCell>Title</TableCell>
-                <TableCell>Description</TableCell>
-                <TableCell>State</TableCell>
-                <TableCell align='center'>Created at</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {tableData.map((row => 
-                  <TableRow 
-                    key={row.id}
-                    sx = { { '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell width={600}>{row.title}</TableCell>
-                    <TableCell>{row.description}</TableCell>
-                    <TableCell>{row.state}</TableCell>
-                    <TableCell align='center'>{row.created_at}</TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-        </Table>
-    </TableContainer>
-  )
+    return (
+        <TableContainer component={Paper} sx={{maxHeight: 400}}>
+            <Table aria-label="simple table" stickyHeader>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Title</TableCell>
+                        <TableCell>Description</TableCell>
+                        <TableCell>State</TableCell>
+                        <TableCell align='center'>Created at</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {tableData.map((row =>
+                            <TableRow
+                                key={row.id}
+                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                            >
+                                <TableCell width={600}>{row.title}</TableCell>
+                                <TableCell>{row.description}</TableCell>
+                                <TableCell>{row.state}</TableCell>
+                                <TableCell
+                                    align='center'>{row.created_at}</TableCell>
+                            </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
 }
 
-
-  
 
 export default GitLabIssueDataTable
